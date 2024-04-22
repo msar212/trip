bd3fc46bf6f508531d0c8afe5e0aa029d48bba97
我再吃一碗 <2650714479@qq.com>
2024-04-22 21:05:06 +0800

    tab-bar 基本页面样式实现

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,10 @@
 <script setup>
-
+  import TabBar from '@/components/tab-bar/tab-bar.vue'
 </script>
 
 <template>
   <router-view></router-view>
-  <router-link to="/home">首页</router-link>
-  <router-link to="/favor">收藏</router-link>
-  <router-link to="/order">订单</router-link>
-  <router-link to="/message">消息</router-link>
+  <TabBar/>
 </template>
 
 <style scoped>
---
+++ src/components/tab-bar/tab-bar.vue
@@ -0,0 +1,50 @@
+<script>
+  export default {
+    
+  }
+</script>
+
+<template>
+  <div class="tab-bar">
+    <router-link to="/home">
+      <img src="@/assets/img/tabbar/tab_home.png" alt="">
+      <span>首页</span>
+    </router-link>
+    <router-link to="/favor">
+      <img src="@/assets/img/tabbar/tab_favor.png" alt="">
+      <span>收藏</span>
+    </router-link>
+    <router-link to="/order">
+      <img src="@/assets/img/tabbar/tab_order.png" alt="">
+      <span>订单</span>
+    </router-link>
+    <router-link to="/message">
+      <img src="@/assets/img/tabbar/tab_message.png" alt="">
+      <span>消息</span>
+    </router-link>
+  </div>
+</template>
+
+<style lang="less" scoped>
+  .tab-bar {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 50px;
+    display: flex;
+
+    &>a {
+      display: flex;
+      flex: 1;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      
+      img {
+        width: 34px;
+        height: 34px;
+      }
+    }
+  }
+</style>
